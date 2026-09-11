fd22ebefb10bf0badddd70026eabf8646c0bfac8
gauravmagdum26 <gauravsm.18@gmail.com>
2026-09-11 22:52:26 +0530

    e-commerce frontend

---
+++ project 2/script1.js
@@ -0,0 +1,18 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+
+    slides[index].classList.add('active');
+}
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+}
+showSlide(currentIndex);
+setInterval(nextSlide, 4000)
